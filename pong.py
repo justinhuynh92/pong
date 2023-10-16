f84e0046117e85b78455e09f0e11a85f6fb1ff2e
Justin Huynh <justinhuynh639@gmail.com>
2023-10-16 12:10:12 -0700

    regulate speed of game

--- pong.py
+++ pong.py
@@ -6,11 +6,17 @@ WIDTH, HEIGHT = 700, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pong")
 
+# define frames per second
+FPS = 60
+
 # main loop to display the window
 def main():
     run = True
+    # regulate the frame rate of the game
+    clock = pygame.time.Clock()
 
     while run:
+        clock.tick(FPS)
         # checks for all events being made within the window
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
