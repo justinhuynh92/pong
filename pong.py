1c1d0829787552eb29c7d56fc0c58ecf5d83e79b
Justin Huynh <justinhuynh639@gmail.com>
2023-10-16 12:03:27 -0700

    first main function made

--- pong.py
+++ pong.py
@@ -1,5 +1,20 @@
 import pygame
+pygame.init()
 
 # set width and height of window
 WIDTH, HEIGHT = 700, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pong")
+
+# main loop to display the window
+def main():
+    run = True
+
+    while run:
+        # checks for all events being made within the window
+        for event in pygame.event.get()
+        if event.type == pygame.QUIT:
+            run = False
+            break
+
+    pygame.quit()
