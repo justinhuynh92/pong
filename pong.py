36f16223c9ef97c51ad6dba884e2ff3a8206cd28
Justin Huynh <justinhuynh639@gmail.com>
2023-10-16 12:05:20 -0700

    got window up

--- pong.py
+++ pong.py
@@ -12,9 +12,12 @@ def main():
 
     while run:
         # checks for all events being made within the window
-        for event in pygame.event.get()
-        if event.type == pygame.QUIT:
-            run = False
-            break
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
 
     pygame.quit()
+
+if __name__ == '__main__':
+    main()
